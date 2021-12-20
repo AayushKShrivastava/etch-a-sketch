62b2d9bb99b58d80ccfce7580aa0698244f2d3a5
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2021-12-20 23:14:06 +0530

    Clear button clearing the grid, and accepting number of pexels from user to create the new grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,27 +1,43 @@
 const container = document.querySelector('#container');
 container.setAttribute('style', ' cursor: pointer; height:600px; width: 600px; background-color: black;display: flex; flex-wrap: wrap');
 
-n = 16;
-for(i = 0; i < n*n; i++){
-    child = document.createElement('div');
-    child.classList.add(`div`);
-    child.setAttribute('style', ` height:${600/n}px; width:${600/n}px; border: 0`);
-    /*child.textContent = 'a';*/
-    container.appendChild(child);
-}
-
-const divs = document.querySelectorAll('.div');
-divs.forEach((div) => {
-    div.addEventListener('mouseout', () =>{
-        /*div.setAttribute('style', 'background-color: black');*/
-        div.classList.add('over');
-    });
-});
+createGrid(16);
+mouseOver();
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () =>{
+    let divs = document.querySelectorAll('.div');
     divs.forEach((div) =>{
-        div.classList.remove('over');
+        container.removeChild(div);
     });
-
+    m = Number(prompt("Enter the Number of Pexels on Both Side (< 100)"));
+    if(m < 100){
+        createGrid(m);
+        mouseOver();
+    }
+    else
+        alert("Please Enter a Number Less Than 100");
 });
+
+//functions
+
+function createGrid(n){
+    for(i = 0; i < n*n; i++){
+        child = document.createElement('div');
+        child.classList.add(`div`);
+        child.setAttribute('style', ` height:${600/n}px; width:${600/n}px; border: 0`);
+        /*child.textContent = 'a';*/
+        container.appendChild(child);
+    }
+}
+
+function mouseOver(){
+    let divs = document.querySelectorAll('.div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseout', () =>{
+        /*div.setAttribute('style', 'background-color: black');*/
+        div.classList.add('over');
+        });
+    });
+}
+
