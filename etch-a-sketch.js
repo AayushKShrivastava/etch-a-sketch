2a5ecd3a424f70f31c6ad487ea96e92f666eab7c
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2021-12-20 19:56:19 +0530

    Created UI

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,27 @@
 const container = document.querySelector('#container');
-container.setAttribute('style', ' height:700px; width: 700px; background-color: aqua;display: flex; flex-wrap: wrap');
-n = 100;
+container.setAttribute('style', ' cursor: pointer; height:600px; width: 600px; background-color: black;display: flex; flex-wrap: wrap');
+
+n = 16;
 for(i = 0; i < n*n; i++){
     child = document.createElement('div');
     child.classList.add(`div`);
-    child.setAttribute('style', `color: red; border: 1px solid black; flex-grow: 1; height:${700/n}px; width:${700/n}px; border: 0`);
+    child.setAttribute('style', ` height:${600/n}px; width:${600/n}px; border: 0`);
     /*child.textContent = 'a';*/
     container.appendChild(child);
 }
 
 const divs = document.querySelectorAll('.div');
 divs.forEach((div) => {
-    div.addEventListener('mouseover', () =>{
+    div.addEventListener('mouseout', () =>{
         /*div.setAttribute('style', 'background-color: black');*/
         div.classList.add('over');
     });
+});
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () =>{
+    divs.forEach((div) =>{
+        div.classList.remove('over');
+    });
+
 });
