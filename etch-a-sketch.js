5df08f826ddc8fb0f2290f10467c7545afabf2ff
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2021-12-22 23:25:14 +0530

    all functionality are working fine

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,39 +1,41 @@
-const container = document.querySelector('#container');
+const container = document.getElementById('container');
 container.setAttribute('style', ' cursor: pointer; height:600px; width: 600px; background-color: black;display: flex; flex-wrap: wrap');
 
-//createGrid(16);
-//mouseOverDraw();
+const mode = document.querySelector('.mode');
+
+createGrid(16);
+
+const pexelInput = document.querySelector('input');
+pexelInput.value = 16;
+pexelInput.addEventListener('change', () =>{
+    let divs = document.querySelectorAll('.div');
+    divs.forEach((div) =>{
+        container.removeChild(div);
+    })
+    createGrid(pexelInput.value);
+});
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () =>{
+    mode.textContent = 'Cleared';
     let divs = document.querySelectorAll('.div');
     divs.forEach((div) =>{
         container.removeChild(div);
     });
+    createGrid(pexelInput.value);
+    pexelInput.disabled = false;
 });
 
 const draw = document.querySelector('.draw');
-const pexelInput = document.querySelector('input');
-pexelInput.value = 16;
 draw.addEventListener('click', () => {
-    //m = Number(prompt("Enter the Number of Pexels on Both Side (< 100)", "16"));
-    
-    if(pexelInput.value == 0){
-        createGrid(16);
-    }
-    else if(pexelInput.value < 100){
-        createGrid(pexelInput.value);
-    }
-    else{
-        alert("Please Enter a Number Less Than 100");
-        pexelInput.value = 16;
-        createGrid(16);
-    }
+    mode.textContent = 'Draw Mode';
+    pexelInput.disabled = true;
     mouseOverDraw();
 });
 
 const erase = document.querySelector('.erase');
 erase.addEventListener('click', () => {
+    mode.textContent = 'Erase Mode';
     mouseOverErase();
 })
 
@@ -44,7 +46,6 @@ function createGrid(n){
         child = document.createElement('div');
         child.classList.add(`div`);
         child.setAttribute('style', ` height:${600/n}px; width:${600/n}px; border: 0`);
-        /*child.textContent = 'a';*/
         container.appendChild(child);
     }
 }
@@ -53,9 +54,9 @@ function mouseOverDraw(){
     let divs = document.querySelectorAll('.div');
     divs.forEach((div) => {
         div.addEventListener('mouseout', () =>{
-        /*div.setAttribute('style', 'background-color: black');*/
         if(div.classList.contains('overErase'))
             div.classList.remove('overErase');
+        pexelInput.disabled = true;
         div.classList.add('overDraw');
         });
     });
@@ -65,10 +66,10 @@ function mouseOverErase(){
     let divs = document.querySelectorAll('.div');
     divs.forEach((div) => {
         div.addEventListener('mouseout', () =>{
-        /*div.setAttribute('style', 'background-color: black');*/
         if(div.classList.contains('overDraw'))
             div.classList.remove('overDraw');
-        div.classList.add('overErase');
+        if(document.querySelectorAll('.overDraw').length == 0)
+            pexelInput.disabled = false;
         });
     });
 }
